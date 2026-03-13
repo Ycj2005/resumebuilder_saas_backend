b9ca3741ceb9650bcb26579cac909c3a21ec195e
Yash Jadhav <yj0034046@gmail.com>
2026-03-13 16:34:39 +0530

    resolve deployment issue

--- src/routes/subscribe.route.js
+++ src/routes/subscribe.route.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Router } from "express";
 import authMiddleware from "../middleware/auth.js"; // <- add .js
 import { createPayment } from "../controllers/subscription.logic.js"; // <- add .js
@@ -8,14 +7,3 @@ const subscribeRouter = Router();
 subscribeRouter.post("/subscribe", authMiddleware, createPayment);
 
 export default subscribeRouter;
-=======
-import { Router } from "express";
-import authMiddleware from "../middleware/auth.js"; // <- add .js
-import { createPayment } from "../controllers/subscription.logic.js"; // <- add .js
-
-const subscribeRouter = Router();
-
-subscribeRouter.post("/subscribe", authMiddleware, createPayment);
-
-export default subscribeRouter;
->>>>>>> af2f786c9b95dbfa4ae119d5629c4ecc9c6181cc
