ba3837171bca9c19b91607466266de9ecf7ac5b8
Ycj2005 <yj0034046@gmail.com>
2026-04-19 00:13:33 +0530

    Fix database connection and prepare for SaaS

--- src/DB/DB.js
+++ src/DB/DB.js
@@ -1,5 +1,9 @@
 import mongoose from "mongoose";
+import dns from "dns";
 import {DB_PATH} from "../config/env.js";
+
+// Override DNS to use Google's public DNS to avoid ECONNREFUSED on local network querySrv resolutions
+dns.setServers(['8.8.8.8']);
 
 export async function GetDatabaseConnection() {
     try {
